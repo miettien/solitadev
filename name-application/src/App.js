bbc45ee47b150c69603a7d00e6d5d51632f990a0
mietti24 <miro.miettinen@student.tut.fi>
2021-01-24 16:22:04 +0200

    styling

--- name-application/src/App.js
+++ name-application/src/App.js
@@ -4,12 +4,16 @@ import data from './names.json'
 const orderByAmount = [].concat(data.names)
   .sort((a, b) => b.amount - a.amount)
   .map((item, i) =>
-  <div>{item.name} {item.amount}</div>
+  <div id="wrapper">
+    <div id="name">{item.name}</div> 
+    <div id="amount">{item.amount}</div>
+  </div>
+  
 )
 
 function App() {
   return (
-    <div className="App">
+    <div id="App-header">
       <h1>Name Application</h1>
       <div>{orderByAmount}</div>
     </div>
